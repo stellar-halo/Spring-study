bb31c37778996e7dd3ade92ea27fe0833a3fed96
river99 <qkrrkd42@naver.com>
2023-02-21 01:50:30 +0900

    #1 [setting] 프로젝트 기초세팅

---
+++ hello-spring/src/test/java/stellarhalo/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package stellarhalo.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
