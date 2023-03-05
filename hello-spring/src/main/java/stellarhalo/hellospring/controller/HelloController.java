4d866f6e0957d4a4353edb2dd4434638d1499dd7
river99 <qkrrkd42@naver.com>
2023-03-05 15:28:47 +0900

    #3 api

--- hello-spring/src/main/java/stellarhalo/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/stellarhalo/hellospring/controller/HelloController.java
@@ -3,13 +3,57 @@ package stellarhalo.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!!");
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("firstName")String firstName, @RequestParam("secondName")String secondName){
+        Hello hello = new Hello();
+        hello.setFirstName(firstName);
+        hello.setSecondName(secondName);
+        return hello;
+    }
+
+    static class Hello{
+        private String firstName;
+        private String secondName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getSecondName() {
+            return secondName;
+        }
+
+        public void setSecondName(String secondName) {
+            this.secondName = secondName;
+        }
     }
 }
